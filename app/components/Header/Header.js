1e93e1be94ef6b398e103c1d6b66543eb3307868
alex <buralex89@gmail.com>
2018-07-06 12:11:32 +0300

    image loader ubuntu

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -8,7 +8,7 @@ class Header extends React.Component { // eslint-disable-line react/prefer-state
         return (
             <div className="header">
                 <a href="https://twitter.com/flexdinesh">
-                    <img src={Banner} alt="react-redux-boilerplate - Logo"/>
+                    <img src={Banner} alt="react-redux-boilerplate - Logo" />
                 </a>
                 <div className="nav-bar">
                     <Link className="router-link" to="/">
